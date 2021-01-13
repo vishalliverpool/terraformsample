1b351a19a9b132a18185daacde656d269599df04
vishalv <vishalv@qburst.com>
2021-01-13 09:35:02 +0530

    commting

---
+++ loadbalancer.tf
@@ -0,0 +1,81 @@
+resource "aws_iam_role" "ecs-instance-role" {
+    name                = "ecs-instance-role"
+    path                = "/"
+    assume_role_policy  = "${data.aws_iam_policy_document.ecs-instance-policy.json}"
+}
+
+data "aws_iam_policy_document" "ecs-instance-policy" {
+    statement {
+        actions = ["sts:AssumeRole"]
+
+        principals {
+            type        = "Service"
+            identifiers = ["ec2.amazonaws.com"]
+        }
+    }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-instance-role-attachment" {
+    role       = "${aws_iam_role.ecs-instance-role.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "ecs-instance-profile" {
+    name = "ecs-instance-profile"
+    path = "/"
+    roles = ["${aws_iam_role.ecs-instance-role.id}"]
+    provisioner "local-exec" {
+      command = "sleep 10"
+    }
+}
+ 
+
+Step 3: Setup ALB
+Now let's setup an ALB to load balance traffic across all our instances.
+
+ 
+
+application-load-balancer.tf
+
+resource "aws_alb" "ecs-load-balancer" {
+    name                = "ecs-load-balancer"
+    security_groups     = ["${aws_security_group.test_public_sg.id}"]
+    subnets             = ["${aws_subnet.test_public_sn_01.id}", "${aws_subnet.test_public_sn_02.id}"]
+
+    tags {
+      Name = "ecs-load-balancer"
+    }
+}
+
+resource "aws_alb_target_group" "ecs-target-group" {
+    name                = "ecs-target-group"
+    port                = "80"
+    protocol            = "HTTP"
+    vpc_id              = "${aws_vpc.test_vpc.id}"
+
+    health_check {
+        healthy_threshold   = "5"
+        unhealthy_threshold = "2"
+        interval            = "30"
+        matcher             = "200"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = "5"
+    }
+
+    tags {
+      Name = "ecs-target-group"
+    }
+}
+
+resource "aws_alb_listener" "alb-listener" {
+    load_balancer_arn = "${aws_alb.ecs-load-balancer.arn}"
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+        target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+        type             = "forward"
+    }
+}
