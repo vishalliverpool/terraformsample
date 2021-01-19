ceaa0f99e6dcebb57508229dbe2c15d12215eb5c
vishalv <vishalv@qburst.com>
2021-01-19 11:05:08 +0530

    commit

--- private_subnet.tf
+++ private_subnet.tf
@@ -18,9 +18,8 @@ resource "aws_route_table" "prod-private-crt" {
     vpc_id = aws_vpc.my_vpc.id
 
     route {
-        //associated subnet can reach everywhere
+    
         cidr_block = "0.0.0.0/0"
-        //CRT uses this IGW to reach internet
         gateway_id = aws_internet_gateway.prod-igw.id
     }
 
