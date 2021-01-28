76c9bb89179649bcc71c25ae5be11dcc30554814
vishalv <vishalv@qburst.com>
2021-01-28 10:01:01 +0530

    modified as cpu as variables

--- task-definiton.tf
+++ task-definiton.tf
@@ -1,4 +1,14 @@
+data "template_file" "conatiner-definiton" {
+  template = file(format("./task-definitions/nginx.tpl"))
+  vars = {
+    image         = var.image
+    memory        = var.memory
+    cpu           = var.cpu
+    name          = var.name
+  }
+}
+
 resource "aws_ecs_task_definition" "nginx" {
   family = "nginx"
-  container_definitions = file("task-definitions/nginx.json")
+  container_definitions = data.template_file.conatiner-definiton.rendered
 }
