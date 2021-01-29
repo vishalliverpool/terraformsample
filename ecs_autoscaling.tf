312dda420098a48d7b8ecd731bd963ab28da363f
vishalv <vishalv@qburst.com>
2021-01-29 09:24:20 +0530

    added the autoscaling policy

--- ecs_autoscaling.tf
+++ ecs_autoscaling.tf
@@ -15,3 +15,30 @@ tags = [
     }
 ]
 }
+
+# Attach an autoscaling policy to the spot cluster to target 50% MemoryReservation on the ECS cluster.
+resource "aws_autoscaling_policy" "ecs_cluster_scale_policy" {
+  name = "my-cluster"
+  policy_type = "TargetTrackingScaling"
+  adjustment_type = "ChangeInCapacity"
+  lifecycle {
+    ignore_changes = [
+      adjustment_type
+    ]
+  }
+  autoscaling_group_name = aws_autoscaling_group.ecs-autoscaling-group.name
+
+  target_tracking_configuration {
+    customized_metric_specification {
+      metric_dimension {
+        name = "ClusterName"
+        value = "my-cluster"
+      }
+      metric_name = "MemoryReservation"
+      namespace = "AWS/ECS"
+      statistic = "Average"
+    }
+    target_value = 50.0
+  }
+}
+
